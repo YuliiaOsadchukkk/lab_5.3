cacc738a870b8caaf8811792c12bafcfaa8774b9
YuliiaOsadchukkk <72582368+YuliiaOsadchukkk@users.noreply.github.com>
2020-11-18 03:43:38 +0200

    lab_5.3

---
+++ lab_5.3/Source.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+double p(const double x);
+int main()
+{
+	double qp, qk, z;
+	int n;
+	cout << "qp = "; cin >> qp;
+	cout << "qk = "; cin >> qk;
+	cout << "n = "; cin >> n;
+	double dq = (qk - qp) / n;
+	double q = qp;
+	while (q <= qk)
+	{
+		z = p(2 * q + 1) + pow(p(q * q - 1), 2) + sqrt(p(1));;
+
+		cout << q << " " << z << endl;
+		q += dq;
+	}
+	return 0;
+}
+double p(const double x)
+{
+	if (abs(x) >= 1)
+		return (sin(x) + cos(x)) / (sin(x) * sin(x) + exp(x));
+	else
+	{
+		double S = 0;
+		int j = 0;
+		double a = 1;
+		S = a;
+		do
+		{
+			j++;
+			double R = -1*x * x / ((2. * j - 1) * 2. * j);
+			a *= R;
+			S += a;
+		} while (j < 4);
+		return (sin(x) * S);
+	}
+}
